6c474b68329fa33fbfd17e588afd1f6eba78fedb
jamesread <contact@jwread.com>
2016-10-28 16:30:03 +0100

    .

--- make.sh
+++ make.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-rm -rf pkg 
-mkdir -p pkg
-
 buildid -n
 buildid -qf rpmmacro -W .buildid.rpmmacro
 
@@ -11,6 +8,9 @@ mkdir -p $BUILD_DIR
 
 cp -r .buildid .buildid.rpmmacro src var $BUILD_DIR/
 
-zip -r upsilon-custodian.zip $BUILD_DIR
+rm -rf build/distributions/
+mkdir -p build/distributions/
+
+zip -r build/distributions/upsilon-custodian.zip $BUILD_DIR
 
 rm -rf $BUILD_DIR
