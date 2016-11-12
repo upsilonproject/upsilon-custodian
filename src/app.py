bedd6b6a3b2a1ccd3260bcc53e5e6c209bf7bf68
jamesread <contact@jwread.com>
2016-11-12 23:55:10 +0000

    .

--- src/app.py
+++ src/app.py
@@ -67,7 +67,7 @@ class DatabaseConnection():
                 return self.execute(query, [serviceId]) 
 
 	def getNode(self, nodeId):
-		return self.execute("SELECT s.name AS nodeId, n.name FROM nodes n WHERE n.id = %s LIMIT 1", [nodeId])
+		return self.execute("SELECT n.id AS nodeId, n.name FROM nodes n WHERE n.id = %s LIMIT 1", [nodeId])
 
 
 class MessageHandler():
